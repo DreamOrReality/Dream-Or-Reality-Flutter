41e12aaaac398592599adffb28212ec4d0dbea9a
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-25 09:29:52 +0900

    feat. 테스트 배너 생성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dream_or_reality/theme/color.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +8,50 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('홈'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // 상단 성향 테스트 배너
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+              color: primaryColor,
+              //텍스트와 이미지의 배치 가로
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //TODO: font 적용할 것
+                      Text(
+                        '나는 어떤 성향의\n개발자/디자이너 일까?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '👉 지금 테스트 하러가기!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Image.asset(
+                    width: 150,
+                    'assets/img/illustration/developer.png',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/theme/color.dart
@@ -0,0 +1,3 @@
+import 'dart:ui';
+
+var primaryColor = const Color(0xFFF76707);
