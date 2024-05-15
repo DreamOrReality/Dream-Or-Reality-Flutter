200dd2e95d029cab5cb1cf4eb79fd28c5f45ba11
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-05-15 15:16:12 +0900

    feat. 회고록 작성 텍스트 필드 생성 및 스타일 설정

--- lib/screens/memoir/write_memoir_screen.dart
+++ lib/screens/memoir/write_memoir_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../theme/color.dart';
+
 class WriteMemoirScreen extends StatelessWidget {
   final DateTime selectedDate;
 
@@ -12,11 +14,35 @@ class WriteMemoirScreen extends StatelessWidget {
         title: const Text("회고록 작성"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Text(
-              "${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일"),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 80),
+            // 선택된 날짜를 불러옴
+            Text(
+              "${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일",
+              style: const TextStyle(
+                fontSize: 20,
+                color: Color(0xFFB3B3B3),
+              ),
+            ),
+            const SizedBox(height: 40),
+            const TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              style: TextStyle(fontSize: 18),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.symmetric(horizontal: 50),
+                hintText: '오늘 하루를 기록해보세요.',
+                hintStyle: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
