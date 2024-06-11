31e3c01fffdc43d5e0d518e9a254e711b0e63130
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-06-11 09:37:49 +0900

    feat. 매개변수화

--- lib/screens/study/study_detail_screen.dart
+++ lib/screens/study/study_detail_screen.dart
@@ -22,9 +22,14 @@ class _StudyDetailScreenState extends State<StudyDetailScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            buildDetailHeader(),
-            buildDetailInfo(),
+            buildDetailHeader(context, 'IT 서비스 공모전에 참여하실 디자인과 분을 모십니다!', '김햄지',
+                '상금이 500만원인 공모전에 참여하실 디자이너분을 모집합니다. 저희는 1주일에 2번 화상회의를 합니다.\n\n저희 팀은 각종 서비스 공모전에서 시상을 할 만큼 능력있는 개발자로 꾸려져 있으나 현재 디자인 인력이 부족한 팀입니다. 피그마를 잘 다룰줄 알며, 어느 정도 기본기가 탄탄한 디자이너를 구합니다.\n\n댓글 남겨주시면 연락드리겠습니다.'),
+            buildDetailInfo(context, '2024-08-24', 3, 'UI/UX 디자이너'),
             buildCommentSection(),
+            buildShowCommentSection(context, '지안땅', '2024-06-11 12:40:32',
+                '저 디자인 쌈@뽕합니다! s2217@e-mirim.hs.kr 컨택 기다리겠습니다!'),
+            buildShowCommentSection(context, '지안땅', '2024-06-11 12:40:32',
+                '저 디자인 쌈@뽕합니다! s2217@e-mirim.hs.kr 컨택 기다리겠습니다!'),
           ],
         ),
       ),
@@ -32,7 +37,8 @@ class _StudyDetailScreenState extends State<StudyDetailScreen> {
   }
 
   // 제목 / 작성자 / 내용을 출력
-  Widget buildDetailHeader() {
+  Widget buildDetailHeader(
+      BuildContext context, String title, String username, String contents) {
     return Container(
       decoration: BoxDecoration(
         border: Border(bottom: BorderSide(color: strokeColor, width: 2)),
@@ -42,14 +48,14 @@ class _StudyDetailScreenState extends State<StudyDetailScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            'IT 서비스 공모전에 참여하실 디자인과 분을 모십니다!',
+            '$title',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 5),
-          Text('김햄지', style: TextStyle(fontSize: 17)),
+          Text('$username', style: TextStyle(fontSize: 17)),
           SizedBox(height: 20),
           Text(
-            '상금이 500만원인 공모전에 참여하실 디자이너분을 모집합니다. 저희는 1주일에 2번 화상회의를 합니다.\n\n저희 팀은 각종 서비스 공모전에서 시상을 할 만큼 능력있는 개발자로 꾸려져 있으나 현재 디자인 인력이 부족한 팀입니다. 피그마를 잘 다룰줄 알며, 어느 정도 기본기가 탄탄한 디자이너를 구합니다.\n\n댓글 남겨주시면 연락드리겠습니다.',
+            '$contents',
           ),
         ],
       ),
@@ -57,7 +63,8 @@ class _StudyDetailScreenState extends State<StudyDetailScreen> {
   }
 
   // 기한 / 인원 / 태그를 출력
-  Widget buildDetailInfo() {
+  Widget buildDetailInfo(
+      BuildContext context, String deadline, int recruit, String tag) {
     return Container(
       padding: EdgeInsets.all(25),
       decoration: BoxDecoration(
@@ -68,7 +75,7 @@ class _StudyDetailScreenState extends State<StudyDetailScreen> {
         children: [
           buildInfoSection(
             title: '기한',
-            content: Text('8월 25일'),
+            content: Text('$deadline'),
           ),
           SizedBox(height: 20),
           buildInfoSection(
@@ -78,7 +85,7 @@ class _StudyDetailScreenState extends State<StudyDetailScreen> {
                 Icon(Icons.person, size: 20, color: primaryColor),
                 SizedBox(width: 15),
                 Text(
-                  '3',
+                  '$recruit',
                   style: TextStyle(color: primaryColor),
                 ),
               ],
@@ -93,7 +100,7 @@ class _StudyDetailScreenState extends State<StudyDetailScreen> {
                 color: primaryColor.withOpacity(0.2),
                 borderRadius: BorderRadius.circular(5),
               ),
-              child: Text('UI/UX 디자이너', style: TextStyle(color: primaryColor)),
+              child: Text('$tag', style: TextStyle(color: primaryColor)),
             ),
           ),
         ],
@@ -103,16 +110,19 @@ class _StudyDetailScreenState extends State<StudyDetailScreen> {
 
   // 댓글 작성 란
   Widget buildCommentSection() {
-    return Padding(
+    return Container(
       padding: EdgeInsets.all(25),
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: strokeColor, width: 2)),
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             '댓글 작성',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 17),
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 15),
           Row(
             children: [
               Expanded(
@@ -164,6 +174,32 @@ class _StudyDetailScreenState extends State<StudyDetailScreen> {
               ),
             ],
           ),
+        ],
+      ),
+    );
+  }
+
+  // 유저들의 댓글 출력
+  Widget buildShowCommentSection(
+      BuildContext context, String username, String date, String comments) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: strokeColor, width: 1)),
+      ),
+      padding: EdgeInsets.all(25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('$username', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('$date', style: TextStyle(color: secondTextColor))
+            ],
+          ),
+          SizedBox(height: 10),
+          Text('$comments'),
         ],
       ),
     );
