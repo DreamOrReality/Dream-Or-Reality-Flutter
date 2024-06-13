943ccf1633e5bcbb682995f04e96903cb2abcc15
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-06-13 12:17:04 +0900

    style. 학과 선택 화면 스타일링

--- lib/screens/test/select_screen.dart
+++ lib/screens/test/select_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dream_or_reality/theme/color.dart';
 
 import 'question_screen.dart';
 
@@ -9,38 +10,83 @@ class SelectScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('개발자 / 디자이너 선택'),
+        title: const Text('학과 선택'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // 개발자 선택 버튼
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          const QuestionScreen(type: 'developer')),
-                );
-              },
-              child: const Text('개발자'),
-            ),
-            // 디자이너 선택 버튼
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          const QuestionScreen(type: 'designer')),
-                );
-              },
-              child: const Text('디자이너'),
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // 개발자 선택 버튼
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const QuestionScreen(type: 'developer')),
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: strokeColor, width: 1.5),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        '개발과',
+                        style: TextStyle(
+                            fontFamily: 'PartialSansKR', color: secondaryColor),
+                      ),
+                      SizedBox(height: 10),
+                      Image.asset(
+                        'assets/img/illustration/developer.png',
+                        width: 120,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 25),
+              // 디자이너 선택 버튼
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const QuestionScreen(type: 'designer')),
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: strokeColor, width: 1.5),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        '디자인과',
+                        style: TextStyle(
+                            fontFamily: 'PartialSansKR', color: secondaryColor),
+                      ),
+                      SizedBox(height: 10),
+                      Image.asset(
+                        'assets/img/illustration/designer.png',
+                        width: 120,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
